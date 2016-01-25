a3f1677506fdeb9302f406c08d0708cdffa4b429
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-01-25 14:54:40 -0600

    website

--- New Unity Project/Assets/Balls.cs
+++ New Unity Project/Assets/Balls.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Balls : MonoBehaviour {
 
+    
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Translate(1f*Time.deltaTime,0f,0f);
+        transform.Translate(0f, 0f, Time.deltaTime * 1.0f);
+
+
+        
 	}
 }
