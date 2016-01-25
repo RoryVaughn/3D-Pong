30bc536f63f9329631771776f0668fca4c7558f7
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-01-25 15:00:04 -0600

    Website round 2

---
+++ Move/Move/Assets/Scripts/Script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Script : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector3 targetpos = Vector3.zero;
+
+    bool on = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("space"))
+        { on = true;  }
+
+        if (Input.GetKeyUp("space"))
+        { on = false; }
+
+        if (on == true)
+        {
+            stuff();
+        }
+
+    }
+
+    void stuff()
+    {
+        Vector3 a = transform.position;
+        transform.position += targetpos - a * 0.000000002f * Time.deltaTime;
+    }
+
+}
+ 
